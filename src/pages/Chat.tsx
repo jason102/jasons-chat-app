13dd2bd0fe6a646597a47f4085a50faf20ad84cc
Jason Stine <lostlaowai@outlook.com>
2022-03-06 23:01:31 +0800

    refactor: remove chatcontext

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ChatPane from '../components/chat-panel/ChatPane';
 import UsersList from '../components/user-list/UsersList';
-import ChatProvider from '../context/ChatContext';
 import { UsersAndChatContainer } from './Chat.styles';
 
-const Chat: React.FC = () => {
-  return (
-    <ChatProvider>
-      <UsersAndChatContainer>
-        <UsersList />
-        <ChatPane />
-      </UsersAndChatContainer>
-    </ChatProvider>
-  );
-};
+const Chat: React.FC = () => (
+  <UsersAndChatContainer>
+    <UsersList />
+    <ChatPane />
+  </UsersAndChatContainer>
+);
 
 export default Chat;
