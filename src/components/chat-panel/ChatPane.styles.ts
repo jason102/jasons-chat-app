47e68f6ad611ea7449ce59d2c1a7e048db2ac0fb
sam.or <sam.or@lalamove.com>
2022-04-04 21:05:50 +0800

    styling for scrollable message component

--- src/components/chat-panel/ChatPane.styles.ts
+++ src/components/chat-panel/ChatPane.styles.ts
@@ -4,7 +4,6 @@ export const ChatContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
-  height: 93vh;
 `;
 
 export const UserToChatWithName = styled.div`
--- src/pages/Chat.styles.ts
+++ src/pages/Chat.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const UsersAndChatContainer = styled.div`
   display: flex;
   flex: 1;
+  overflow-y: auto;
 `;
 
 export const SelectUserToChatWith = styled.div`
